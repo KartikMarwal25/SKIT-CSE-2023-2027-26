beb2cfc12ca5309194fb34592d6d43c0ef9da373
Keshav0419 <keshavbheel1904@gmail.com>
2026-09-17 16:20:50 +0530

    feat(web): add the certificate/:id route

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { InstitutionLayout } from './pages/institution/InstitutionLayout.jsx';
 import { IssuePage } from './pages/institution/IssuePage.jsx';
 import { RegistryPage } from './pages/institution/RegistryPage.jsx';
+import { CertificateDetailPage } from './pages/institution/CertificateDetailPage.jsx';
 import { GalleryPage } from './pages/student/GalleryPage.jsx';
 
 export function App() {
@@ -12,6 +13,7 @@ export function App() {
           <Route index element={<Navigate to="registry" replace />} />
           <Route path="registry" element={<RegistryPage />} />
           <Route path="issue" element={<IssuePage />} />
+          <Route path="certificate/:id" element={<CertificateDetailPage />} />
         </Route>
         <Route path="/me" element={<GalleryPage />} />
       </Routes>
